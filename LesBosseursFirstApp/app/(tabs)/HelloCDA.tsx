2b38a4af896b8eede20cbea5a8c07a07dcc62e51
Dima Dalloul <dima.dalloul@univ-grenoble-alpes.fr>
2026-04-03 11:17:26 +0200

    Première application React Native pour CESI_CDA25

---
+++ LesBosseursFirstApp/app/(tabs)/HelloCDA.tsx
@@ -0,0 +1,58 @@
+import { Image } from 'expo-image';
+import { Platform, StyleSheet } from 'react-native';
+
+import { Collapsible } from '@/components/ui/collapsible';
+import { ExternalLink } from '@/components/external-link';
+import ParallaxScrollView from '@/components/parallax-scroll-view';
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import { IconSymbol } from '@/components/ui/icon-symbol';
+import { Fonts } from '@/constants/theme';
+
+export default function TabTwoScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#ffffff', dark: '#ffffff' }}
+      headerImage={
+        <Image
+          source={require('@/assets/images/CESI_logo.jpg')}
+          style={styles.reactLogo}
+        />
+      }>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText
+          type="title"
+          style={{
+            fontFamily: Fonts.rounded,
+          }}>
+          Bienvenue à vous dans le joli monde de React Native!
+        </ThemedText>
+      </ThemedView>
+      
+      <ThemedText>Cette première page vous est dédiée chers CDA 25 du CESI Grenoble :-)</ThemedText>
+      
+      <ThemedText>N'hésitez pas à découvrir les autres pages!</ThemedText>
+      
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: '#808080',
+    bottom: -90,
+    left: -35,
+    position: 'absolute',
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
